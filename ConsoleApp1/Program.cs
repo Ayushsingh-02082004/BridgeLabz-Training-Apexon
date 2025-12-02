c872e50a507c1e5c8a4951baa99758b2e1b5f406
Ayush Singh <ayushsingh02082004@gmail.com>
2025-12-02 15:40:53 +0530

    my first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 45, 34, 56 };
+
+            for(int i = 0;i<arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            
+        }
+    }
+}
