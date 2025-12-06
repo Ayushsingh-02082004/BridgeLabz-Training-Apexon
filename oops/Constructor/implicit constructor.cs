7a15b571c596431a51d0ab63fc1ae06d98281563
Ayush Singh <ayushsingh02082004@gmail.com>
2025-12-06 10:06:25 +0530

    Commiting the constructors file

---
+++ oops/Constructor/implicit constructor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops.Constructor
+{
+    internal class implicit_constructor
+    {
+
+        //implicit constructor is also called default constructor because it initialize all the values on its on because user has not initi
+        //alized those values. it initializes num values to 0 bool as false and string , objects as null.
+        // by default is public.
+
+  
+            public int num;
+            public String text;
+            public bool flag;
+
+           
+        
+    }
+}
---
+++ oops/Program.cs
@@ -0,0 +1,35 @@
+﻿using oops.Constructor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace oops
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            //implicit_constructor imp = new implicit_constructor();
+            //Console.WriteLine($"{imp.num} {imp.flag} {imp.text}");
+            //Console.WriteLine("hello");
+
+            //parameterizedconstructor constructor = new parameterizedconstructor(10);
+            //Console.ReadKey();
+
+            //userdefinedDefaultConstructor ud = new userdefinedDefaultConstructor();
+            //ud.Display();
+
+            //copyconstructor cp = new copyconstructor(10);
+            //cp.Display();
+            //copyconstructor dp = new copyconstructor(cp);
+            //dp.Display();
+
+            staticConstructor obj = new staticConstructor();  // Triggers static ctor implicitly
+            Console.WriteLine("Main method executed");
+
+        }
+    }
+}
